296e829e21d6f5ce10eee563fe8bdf10e00222c5
League Student <league@iMac-34.attlocal.net>
2022-05-19 18:29:08 -0700

    Tests

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -8,10 +8,30 @@ import java.util.List;
 public class Algorithms {
     
     public static String multiply(int num1, int num2) {
-        return null;
+        return num1 + " x " + num2 +" = " + num1*num2;
     }
     
     public static int findBrokenEgg(List<String> eggs) {
         return 0;
     }
+
+	public static boolean isPrime(int i) {
+		// TODO Auto-generated method stub) {
+		for (int j = 2; j < i; j++) {
+			if(i%j == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean isSquare(int i) {
+		// TODO Auto-generated method stub
+		for (int j = 1; j < i; j++) {
+			if (i/j == j) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
